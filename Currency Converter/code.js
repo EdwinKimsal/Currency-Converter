97f0ceaeca0e4d4fbe0dd3a1c98f10d594477e7a
EdwinKimsal <107333344+EdwinKimsal@users.noreply.github.com>
2022-08-27 16:34:44 -0400

    Add files via upload

--- Currency Converter/code.js
+++ Currency Converter/code.js
@@ -58,3 +58,5 @@ function convert(){
             }
         })
 }
+
+window.addEventListener('keydown',function(e){if(e.keyIdentifier=='U+000A'||e.keyIdentifier=='Enter'||e.keyCode==13){if(e.target.nodeName=='INPUT'&&e.target.type=='text'){e.preventDefault();return false;}}},true);
